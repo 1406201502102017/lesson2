e9efe6a2a4987d94763446b70587c0efe7195b2e
1406201502102017 <bekas.omsk@mail.ru>
2021-11-13 21:41:01 +0600

    дополнен подсчет в class Animal

--- src/main/java/gb/homework/oop/lesson_6/Animal.java
+++ src/main/java/gb/homework/oop/lesson_6/Animal.java
@@ -4,6 +4,7 @@ public class Animal {
     protected String name;
     protected int distanceRun;
     protected int distanceSwim;
+    private static int count = 0;
 
     public String getName() {
         return name;
@@ -23,13 +24,23 @@ public class Animal {
     public void setDistanceSwim(int distanceSwim) {
         this.distanceSwim = distanceSwim;
     }
+    public Animal() {
+         count++;
+    }
+    public static int getCount() {
+        return count;
+    }
     public static void main(String[] args) {
         Cat cat = new Cat("Murzik", 30, 100);
         Dog dog = new Dog("Sharik", 10, 300);
+        int counter = Animal.getCount();
 
         cat.swim();
         cat.run();
         dog.swim();
         dog.run();
+
+    System.out.println("Cats: " + Cat.getCount());
+    System.out.println("Dogs: " + Dog.getCount());
     }
 }
--- src/main/java/gb/homework/oop/lesson_6/Dog.java
+++ src/main/java/gb/homework/oop/lesson_6/Dog.java
@@ -22,4 +22,3 @@ public class Dog extends Animal {
         }
     }
 }
-
